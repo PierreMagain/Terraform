f3663418f7501ed775eb6d6418d8a68f3524f702
Pierre Magain <magainppm@gmail.com>
2024-08-15 18:24:15 +0200

    Main.tf vm_ubuntu on Azure

---
+++ main.tf
@@ -0,0 +1,134 @@
+# Configuration du provider Azure
+provider "azurerm" {
+  features {}
+}
+
+# Création du groupe de ressources
+resource "azurerm_resource_group" "rg" {
+  name     = "Test"
+  location = "Germany West Central"
+}
+
+# Création du réseau virtuel
+resource "azurerm_virtual_network" "vnet" {
+  name                = "Network-test"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Création du sous-réseau
+resource "azurerm_subnet" "subnet" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+# Création de l'IP publique
+resource "azurerm_public_ip" "public_ip" {
+  name                = "testubuntu-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Création de l'interface réseau
+resource "azurerm_network_interface" "nic" {
+  name                = "testubuntu-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "testubuntu-ip-config"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+# Création de la machine virtuelle
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "testubuntu"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "azureuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    name                 = "testubuntu-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_ZRS"
+    disk_size_gb         = 30
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "20.04.202408090"
+  }
+
+  #zone = "1"  # Spécifie la zone, ici "1".
+
+  # Supprime automatiquement les ressources associées à la VM lors de sa suppression
+  lifecycle {
+    ignore_changes = [os_disk]
+  }
+}
+
+# Group de sécurité réseau pour autoriser les ports
+resource "azurerm_network_security_group" "nsg" {
+  name                = "testubuntu-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "allow_ssh"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["22"]
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow_http"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["80"]
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow_https"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["443"]
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Association du NSG avec l'interface réseau
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
